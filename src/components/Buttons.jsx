f42310a0cef41968fb80f0e798579cc0720ce754
talgat55 <lightx_design@yahoo.com>
2020-05-09 14:13:31 +0600

    add buttons

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import {StoreContext} from "../app";
+
+const Buttons = () => {
+    const store = useContext(StoreContext);
+
+    return (
+        <div className="row">
+            <div className="col-sm">
+                <button type="button" className="btn btn-light align-top" onClick={() => store.updateCount()}>
+                    <i className="fa fa-thumbs-o-up"/>
+                    Like
+                </button>
+            </div>
+            <div className="col-sm">
+                <button type="button" className="btn btn-light" onClick={()=> document.getElementById('comment').focus()}>
+                    <i className="fa fa-comment-o" />
+                    Comment
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Buttons;
