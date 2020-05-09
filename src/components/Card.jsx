3b67bb06b9a7047aa4dab84af3ec799d3cd4d22c
talgat55 <lightx_design@yahoo.com>
2020-05-09 21:39:40 +0600

    add card

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Count from "./Count";
+import Buttons from "./Buttons";
+import {useObserver} from 'mobx-react';
+
+const Card = () => {
+
+    return (
+        useObserver(() => {
+            <div className="card">
+                <button className="btn btn-light"
+                        onClick={() => {
+                    console.log('test');
+                }}
+                >
+                    <i className="fa fa-chevron-right"/>
+                </button>
+                <Count/>
+                <div className="card-body">
+                    <Buttons/>
+                </div>
+            </div>
+        })
+    )
+};
+
+export default  Card;
+
+
